6dc0259ee2df56650072f3347bfcdc5670387ce1
Shahman3 <152794381+Shahman3@users.noreply.github.com>
2024-07-13 15:05:54 -0700

    Delete MERN directory

--- MERN/server/router/contact-router.js
+++
@@ -1,6 +0,0 @@
-const experss = require("express");
-const router = experss.Router();
-const contactForm = require("../controllers/contact-controller");
-
-router.route("/contact").post(contactForm);
-module.exports = router;
--- MERN/server/router/service-router.js
+++
@@ -1,6 +0,0 @@
-const experss = require("express");
-const router = experss.Router();
-const services = require("../controllers/service-controller");
-
-router.route("/service").get(services);
-module.exports = router;
